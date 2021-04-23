ebae54d74563cfdb46056829a842e06e4f7315f1
Mario Souto <soutomarios@gmail.com>
2021-04-23 02:04:33 -0300

    fix: small update

---
+++ pages/api/topic/[topic].js
@@ -0,0 +1,62 @@
+async function getRepositoriesByTopic(topic, currentPage = 1, totalItems = []) {
+    const url = `https://api.github.com/search/repositories?q=${topic}&per_page=10&page=${currentPage}`;
+
+    const response = await fetch(url);
+    const { items } = await response.json();
+    const paginationItems = response.headers.get('link')?.split(',');
+    const nextPageItem = paginationItems?.find((link) => link.includes('rel="next"'));
+    const hasNextURL = Boolean(nextPageItem);
+    const allCurrentItems = [ ...totalItems, ...items ];
+    
+    if(hasNextURL) {
+        return await getRepositoriesByTopic(topic, currentPage + 1, allCurrentItems)
+    }
+
+
+    return allCurrentItems.map((item, index) => {
+        return {
+            index,
+            title: item.full_name,
+            url: item.html_url,
+        }
+    });
+}
+
+
+function xmlFeedView({ title, items }) {
+    function entry(item) {
+        return (
+`
+<item>
+<index>${item.index}</index>
+<title>${item.title}</title>
+<author>item.author</author>
+<description>item.description</description>
+<link rel="alternate" href="${item.url}"/>
+</item>
+`
+        )
+    }
+
+    return (
+`<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0">
+<channel>
+    <title>GitHub Topic: ${title}</title>
+    <author>GitHub Topic: ${title}</author>
+    ${items.map(entry).join('')}
+</channel>
+</rss>`
+    )
+}
+
+
+export default async function handler(req,res) {
+    const { topic } = req.query;
+
+    res.setHeader('Content-Type', 'text/xml')
+
+    const items = await getRepositoriesByTopic(topic);
+
+    res.end(xmlFeedView({ items, title: `Repositories by Topic: "${topic}"` }));
+}
