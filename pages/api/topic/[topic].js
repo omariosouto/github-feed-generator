cd0fb3498143676fb8b462a2f663f94b48acc1e7
Mario Souto <soutomarios@gmail.com>
2021-05-05 16:51:59 -0300

    Update [topic].js

--- pages/api/topic/[topic].js
+++ pages/api/topic/[topic].js
@@ -2,7 +2,7 @@ async function getRepositoriesByTopic(topic, currentPage = 1, totalItems = []) {
     const url = `https://api.github.com/search/repositories?q=${topic}&per_page=100&page=${currentPage}`;
     
     const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://github-feed-generator-omariosouto.vercel.app/';
-    const fetchURL = `${BASE_URL}/api/github?url=${url.replace(/\&/gi, '_____')}&v=2`;
+    const fetchURL = `${BASE_URL}/api/github?url=${url.replace(/\&/gi, '_____')}&v=3`;
     // console.log('/api/topic/[topicname] [fetchURL]', fetchURL);
     const response = await fetch(fetchURL);
     const { data, headers } = await response.json();
